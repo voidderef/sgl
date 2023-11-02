bcf95f4ead0fd9009cc9fe73baefa7172c1fd6c3
voidderef <voidderef@gmail.com>
2023-11-02 19:13:57 +0100

    fix: BemanitoolsGlue log formatting output

--- src/main/cpp/io/bt/BemanitoolsGlue.cpp
+++ src/main/cpp/io/bt/BemanitoolsGlue.cpp
@@ -12,26 +12,62 @@ std::unordered_map<int, std::thread*> BemanitoolsGlue::m_threads;
 
 void BemanitoolsGlue::LogFormatterMisc(const char* module, const char* fmt, ...)
 {
-    // TODO incomplete
-    KS_LOG_DEBUG("[%s] %s", module, fmt);
+    // 64k in bemanitools to allow large logs like IO buffer dumps
+    char buffer[65536];
+
+    va_list args;
+    va_start(args, fmt);
+
+    vsprintf(buffer, fmt, args);
+
+    va_end(args);
+
+    KS_LOG_DEBUG("[%s] %s", module, buffer);
 }
 
 void BemanitoolsGlue::LogFormatterInfo(const char* module, const char* fmt, ...)
 {
-    // TODO incomplete
-    KS_LOG_INFO("[%s] %s", module, fmt);
+    // 64k in bemanitools to allow large logs like IO buffer dumps
+    char buffer[65536];
+
+    va_list args;
+    va_start(args, fmt);
+
+    vsprintf(buffer, fmt, args);
+
+    va_end(args);
+
+    KS_LOG_INFO("[%s] %s", module, buffer);
 }
 
 void BemanitoolsGlue::LogFormatterWarning(const char* module, const char* fmt, ...)
 {
-    // TODO incomplete
-    KS_LOG_WARN("[%s] %s", module, fmt);
+    // 64k in bemanitools to allow large logs like IO buffer dumps
+    char buffer[65536];
+
+    va_list args;
+    va_start(args, fmt);
+
+    vsprintf(buffer, fmt, args);
+
+    va_end(args);
+
+    KS_LOG_WARN("[%s] %s", module, buffer);
 }
 
 void BemanitoolsGlue::LogFormatterFatal(const char* module, const char* fmt, ...)
 {
-    // TODO incomplete
-    KS_LOG_ERROR("[%s] %s", module, fmt);
+    // 64k in bemanitools to allow large logs like IO buffer dumps
+    char buffer[65536];
+
+    va_list args;
+    va_start(args, fmt);
+
+    vsprintf(buffer, fmt, args);
+
+    va_end(args);
+
+    KS_LOG_ERROR("[%s] %s", module, buffer);
 }
 
 int BemanitoolsGlue::ThreadCreate(int (*proc)(void*), void* ctx, uint32_t stackSize, unsigned int priority)
